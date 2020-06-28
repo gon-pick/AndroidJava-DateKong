6ba75777d7d44843b6cb0489b8e6b1a1a94b9e30
gon-pick <opve555@gmail.com>
2020-06-28 19:57:43 +0900

    Delete UI Video

--- app/src/main/java/org/techtown/datekong/FirebaseHelper.java
+++ app/src/main/java/org/techtown/datekong/FirebaseHelper.java
@@ -46,7 +46,7 @@ public class FirebaseHelper {
                     @Override
                     public void onSuccess(Void aVoid) {
                         successCount--;
-                        showToast(activity, "파이어베이스의 게시물이 삭제되었습니다.");
+                        showToast(activity, "게시물을 삭제하였습니다.");
                         storeDelete(id);
                     }
                 }).addOnFailureListener(new OnFailureListener() {
